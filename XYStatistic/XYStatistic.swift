9cd1393249d2c96d028d18ae7994ce1bcc9980ea
yangjianxiang <yangjianxiang@xunyou.com>
2022-06-06 11:05:39 +0800

    统计

--- XYStatistic/XYStatistic.swift
+++ XYStatistic/XYStatistic.swift
@@ -17,9 +17,13 @@ final public class XYStatistic {
     var eventAPI = ""
     var user = XYStatsPropertyUser.init()
     var event = XYStatsPropertyEvent.init()
+    var datas = [[String : Any]].init()
+    let queue = OperationQueue()
+    let lock = NSLock.init()
     public static let shared = XYStatistic()
     
     public init() {
+        self.queue.maxConcurrentOperationCount = 1
     }
     
     public func initialize(propertyAPI: String, eventAPI: String) {
@@ -35,23 +39,29 @@ final public class XYStatistic {
         self.user = user
         let parameters = user.toJSON()
         
-        AF.request(
-            self.propertyAPI,
-            method: .post,
-            parameters: parameters,
-            encoding: JSONEncoding.default
-        )
-        .validate()
-        .responseDecodable(of: XYStatsResponse.self) { response in
-            switch response.result {
-            case let .success(xy):
-                if xy.code != 0 {
-                    logger.print("上报失败（user）: \(xy.message ?? "")", module: .statistic)
-                } else {
-                    logger.print(parameters.jsonString() ?? "", module: .statistic)
+        self.queue.addOperation {
+            self.lock.lock()
+            logger.print("update线程: \(Thread.current)开始", module: .statistic)
+            AF.request(
+                self.propertyAPI,
+                method: .post,
+                parameters: parameters,
+                encoding: JSONEncoding.default
+            )
+            .validate()
+            .responseDecodable(of: XYStatsResponse.self) { response in
+                switch response.result {
+                case let .success(xy):
+                    if xy.code != 0 {
+                        logger.print("上报失败（user）: \(xy.message ?? "")", module: .statistic)
+                    } else {
+                        logger.print(parameters.jsonString() ?? "", module: .statistic)
+                    }
+                case let .failure(error):
+                    logger.print("上报错误（user）: \(error)", module: .statistic)
                 }
-            case let .failure(error):
-                logger.print("上报错误（user）: \(error)", module: .statistic)
+                logger.print("update线程: \(Thread.current)结束", module: .statistic)
+                self.lock.unlock()
             }
         }
     }
@@ -60,18 +70,17 @@ final public class XYStatistic {
         self.event = event
     }
      
-    public func trackEvent(_ event: XYStatsEvent, _ customInfo: [String: Any]? = nil) {
+    public func trackEvent(_ event: XYStatsEvent) {
         guard !self.eventAPI.isEmpty else {
             logger.print("未设置eventAPI！！！", module: .statistic)
             return
         }
         var parameters = event.properties
         parameters += self.event.toJSON()
-        if let customInfo = customInfo {
-            parameters += customInfo
-        }
         
-        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+        self.queue.addOperation {
+            self.lock.lock()
+            logger.print("trackEvent线程: \(Thread.current)开始", module: .statistic)
             AF.request(
                 self.eventAPI,
                 method: .post,
@@ -90,6 +99,8 @@ final public class XYStatistic {
                 case let .failure(error):
                     logger.print("上报错误（event）: \(error)", module: .statistic)
                 }
+                logger.print("trackEvent线程: \(Thread.current)结束", module: .statistic)
+                self.lock.unlock()
             }
         }
     }
